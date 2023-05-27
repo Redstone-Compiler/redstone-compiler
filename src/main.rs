402b33497cefa3c698223b7ff277eb5e3c54e07b
rollrat <rollrat.cse@gmail.com>
2023-05-28 00:05:35 +0900

    add some stubs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+pub struct Constraints {}
+
+#[derive(Clone, Debug, Copy)]
+pub enum BlockKind {
+    Cobble,
+    Redstone,
+    Torch,
+    Repeater,
+    RedstoneBlock,
+}
+
+pub enum BlockState {
+    None,
+    Bottom,
+    East,
+    West,
+    South,
+    North,
+}
+
+pub struct Block {
+    pub kind: BlockKind,
+    pub state: BlockState,
+}
+
+pub struct Gate {}
+
+pub struct LogicCircuit {}
+
+pub struct ProcessingElement {}
+
 fn main() {
     println!("");
 }
