79e8cb0f8c3005eeb4fc9057c504e8b519c3646e
rollrat <rollrat.cse@gmail.com>
2023-06-18 16:36:10 +0900

    design: add restone state enum

--- src/common/block.rs
+++ src/common/block.rs
@@ -2,8 +2,9 @@ use crate::common::{DimSize, Position};
 
 use super::world::World;
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Default, Copy, Clone)]
 pub enum Direction {
+    #[default]
     None,
     Bottom,
     Top,
@@ -19,19 +20,32 @@ impl Direction {
     }
 }
 
+#[derive(Default, Clone, Debug, Copy)]
+pub enum RedstoneState {
+    #[default]
+    None,
+    EastWest,
+    SouthNorth,
+    EastNorth,
+    EastSouth,
+    WestNorth,
+    WestSouth,
+}
+
 // 블럭의 종류
-#[derive(Clone, Debug, Copy)]
+#[derive(Default, Clone, Debug, Copy)]
 pub enum BlockKind {
+    #[default]
     Air,
     Cobble {
-        is_on: bool,
+        on_count: usize,
     },
     Switch {
         is_on: bool,
     },
     Redstone {
-        is_enable: bool,
-        is_on: bool,
+        on_count: usize,
+        state: RedstoneState,
         strength: usize,
     },
     Torch {
@@ -51,11 +65,73 @@ pub struct Block {
     pub direction: Direction,
 }
 
+impl Block {
+    pub fn count_up(&mut self) -> eyre::Result<usize> {
+        let cnt;
+
+        self.kind = match self.kind {
+            BlockKind::Cobble { on_count } => {
+                cnt = on_count + 1;
+
+                BlockKind::Cobble { on_count: cnt }
+            }
+            BlockKind::Redstone {
+                on_count,
+                strength,
+                state,
+            } => {
+                cnt = on_count + 1;
+
+                BlockKind::Redstone {
+                    on_count: cnt,
+                    state,
+                    strength,
+                }
+            }
+            _ => eyre::bail!("You cannot count up on {:?}!", self.kind),
+        };
+
+        Ok(cnt)
+    }
+
+    pub fn count_down(&mut self) -> eyre::Result<usize> {
+        let cnt;
+
+        self.kind = match self.kind {
+            BlockKind::Cobble { on_count } => {
+                eyre::ensure!(on_count > 0, "On count must higher than zero");
+
+                cnt = on_count - 1;
+
+                BlockKind::Cobble { on_count: cnt }
+            }
+            BlockKind::Redstone {
+                on_count,
+                strength,
+                state,
+            } => {
+                eyre::ensure!(on_count > 0, "On count must higher than zero");
+
+                cnt = on_count - 1;
+
+                BlockKind::Redstone {
+                    on_count: cnt,
+                    state,
+                    strength,
+                }
+            }
+            _ => eyre::bail!("You cannot count down on {:?}!", self.kind),
+        };
+
+        Ok(cnt)
+    }
+}
+
 impl Default for Block {
     fn default() -> Self {
         Self {
-            kind: BlockKind::Air,
-            direction: Direction::None,
+            kind: Default::default(),
+            direction: Default::default(),
         }
     }
 }
