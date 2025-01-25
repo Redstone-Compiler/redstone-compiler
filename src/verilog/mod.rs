ad3e02bf394f6caff401f1626f2c00ad3d682ccf
rollrat <rollrat.cse@gmail.com>
2025-01-25 13:11:02 +0900

    Disable verilog

--- src/verilog/mod.rs
+++ src/verilog/mod.rs
@@ -1,15 +1,15 @@
-use std::{collections::HashMap, path::PathBuf};
+// use std::{collections::HashMap, path::PathBuf};
 
-use sv_parser::{parse_sv, SyntaxTree};
+// use sv_parser::{parse_sv, SyntaxTree};
 
-pub fn load(path: &PathBuf) -> eyre::Result<SyntaxTree> {
-    let defines = HashMap::new();
-    let includes: Vec<PathBuf> = Vec::new();
+// pub fn load(path: &PathBuf) -> eyre::Result<SyntaxTree> {
+//     let defines = HashMap::new();
+//     let includes: Vec<PathBuf> = Vec::new();
 
-    let result = parse_sv(path, &defines, &includes, false, false);
-    let Ok((syntax_tree, _)) = result else {
-        eyre::bail!("System-verilog input parse err! {}", result.err().unwrap());
-    };
+//     let result = parse_sv(path, &defines, &includes, false, false);
+//     let Ok((syntax_tree, _)) = result else {
+//         eyre::bail!("System-verilog input parse err! {}", result.err().unwrap());
+//     };
 
-    Ok(syntax_tree)
-}
+//     Ok(syntax_tree)
+// }
